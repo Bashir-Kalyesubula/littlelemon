026c1d7972382f79f7a9c03c35baa30f387cc99b
Bashir-Kalyesubula <bashirkalyesubula2@gmail.com>
2023-03-20 13:17:13 -0500

    added authentication to tableAPI

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'restaurant',
-    'rest_framework',
+    'rest_framework', 
+    'djoser',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -95,6 +97,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
+REST_FRAMEWORK = { 
+        'DEFAULT_AUTHENTICATION_CLASSES':( 
+            
+            'rest_framework.authentication.TokenAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -121,3 +130,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DJOSER = {"USER_ID_FIELD":"username"} 
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,14 +3,16 @@ from rest_framework import generics
 from rest_framework.decorators import api_view 
 from .serializers import MenuSerializer,BookingSerializer 
 from . models import Bookingtable,Menutable 
-
+from rest_framework.permissions import IsAuthenticated 
 # Create your views here.
 def index(request): 
     return render(request, 'index.html', {}) 
 
-class BookingView(generics.ListCreateAPIView):
+class BookingView(generics.ListCreateAPIView): 
+    permission_classes = [IsAuthenticated]
     queryset = Bookingtable.objects.all()  
     serializer_class = BookingSerializer  
+    
     
     
 class MenuItemsView(generics.ListCreateAPIView):
