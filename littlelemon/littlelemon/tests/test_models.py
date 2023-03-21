1670517c94917fdedfab3d191b78714a6b07ac72
Bashir-Kalyesubula <bashirkalyesubula2@gmail.com>
2023-03-21 13:23:56 -0500

    view and model tests

--- littlelemon/littlelemon/tests/test_models.py
+++ littlelemon/littlelemon/tests/test_models.py
@@ -1,11 +1,16 @@
 from django.test import TestCase 
 from restaurant.models import Menutable,Bookingtable 
+from rest_framework.test import APIClient 
 
 class MenuItemTest(TestCase): 
+    def setUp(self):
+        self.client = APIClient
+        self.menu1 = Menutable.objects.create(title="Rice and Beans",price="6.90",inventory=12) 
+        self.menu2 = Menutable.objects.create(title="Matooke",price="8.95",inventory=13) 
+        
     
     def test_get_item(self):
-        item = Menutable.objects.create(title="Matooke", price=8.95,inventory=13) 
-        itemstr = item 
+        menu3 = Menutable.objects.create(title="Peas salad", price="3.50",inventory=34) 
+        anticipated_value = f"{menu3.title}:{menu3.price}"
+        self.assertEqual(str(menu3),anticipated_value)
         
-        
-        self.assertEqual(item, "Rice and Beans : 6.90,12")
